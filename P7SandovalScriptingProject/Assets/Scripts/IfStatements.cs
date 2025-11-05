070d16b80bf3bdee73dac2fb4e93eec66c22743c
saucersauros <733434@student.dusd.net>
2025-11-05 14:34:01 -0800

    I made a new script
    I made an if statement script

---
+++ P7SandovalScriptingProject/Assets/Scripts/IfStatements.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    float coffeeTempature = 85.0f;
+    float hotLimitTempature = 70.0f;
+    float coldLimitTempature = 40.0f;
+
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+            TemperatureTest();
+
+        coffeeTempature -= Time.deltaTime * 5f;
+
+    }
+
+
+    void TemperatureTest()
+    {
+        if(coffeeTempature > hotLimitTempature)
+        {
+            print("my Chicken is hot");
+        }
+
+        else if(coffeeTempature < coldLimitTempature)
+        {
+            print("my Chicken is cold");
+        }
+
+        else
+        {
+            print("my chicken is just right");
+        }
+
+    }
+}
